94ebb694687db729bf70078c610e817f1fc1d2d8
hezhilong <1092501244@qq.com>
2021-02-02 18:03:18 +0800

    1

--- electron/main.js
+++ electron/main.js
@@ -19,7 +19,7 @@ function createWindow() {
   Menu.setApplicationMenu(null);
   win = new BrowserWindow(windowConfig); //创建一个窗口
   win.loadURL(`file://${__dirname}/index.html`); //在窗口内要展示的内容index.html 就是打包生成的index.html
-  //win.webContents.openDevTools(); //开启调试工具
+  win.webContents.openDevTools(); //开启调试工具
   win.on('close', () => {
     //回收BrowserWindow对象
     win = null;
